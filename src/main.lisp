9a16a12739424848aa5338b5610f2cb339a8ea83
Thiru Thirunavukarasu <thiru0130@gmail.com>
2017-07-15 17:34:39 -0400

    Show to specific workspace

--- src/main.lisp
+++ src/main.lisp
@@ -2,3 +2,7 @@
 
 (defun show-wmctrl-help ()
   (r-to-values (run-cmd "wmctrl --help") :strip-data? t))
+
+(defun go-to-workspace (num)
+  "Go to workspace number `num` (1-based index)."
+  (r-to-values (run-cmd (sf "wmctrl -s ~A" (max 0 (1- (or num 1)))))))
